d3f0b49354b25893ade14b9859aff931613ab71b
bdlatter <bdlatter@ncsu.edu>
2021-04-28 15:54:31 -0400

    Added sink mode to testing demo

--- testing.py
+++ testing.py
@@ -2,7 +2,7 @@ from scapy.all import *
 import time
 
 class INTP(Packet):
-    name = "Custom INT Packet"
+    name = "\u001b[100mCustom INT Header\u001b[40m"
     fields_desc = [
         BitField("version", 0, 2),
         BitField("append", 0, 1),
@@ -32,14 +32,44 @@ def test():
     aout.stop()
     print("!~! Input !~!")
     ain.results[0].show()
-    print("!~! Output !~!")
-    # aout.results[0].show()
+    print("!~! Source Output !~!")
+    rxPkt = aout.results[0]
     if len(aout.results) > 0:
-        rxPkt = aout.results[0]
-        # import pdb; pdb.Pdb().set_trace()
-        print(rxPkt[UDP].decode_payload_as(INTP))
+        rxPkt[UDP].decode_payload_as(INTP)
+        print("\u001b[36m")
         rxPkt.show()
+        print("\u001b[0m")
+    wrpcap("int1.cap", rxPkt)
+
+    aout2 = AsyncSniffer(iface="s1-eth2")
+    aout2.start()
+    sendp(rxPkt, iface="s1-eth1")
+    
+    # To stop the sniffer and show summary
+    time.sleep(1)
+    aout2.stop()
+    print("!~! Transit Output !~!")
+    if len(aout.results) > 0:
+        rxPkt = aout2.results[0]
+        rxPkt[UDP].decode_payload_as(INTP)
+        rxPkt[INTP].decode_payload_as(INTP)
+        print("\u001b[95m")
+        rxPkt.show()
+        print("\u001b[0m")
+        wrpcap("int2.cap", rxPkt)
+
+def test2():
+    aout = AsyncSniffer(iface="s1-eth2")
+    int2 = rdpcap("int2.cap")
+
+    aout.start()
+    sendp(int2, iface="s1-eth1")
+    time.sleep(1)
+    aout.stop()
+    aout.results[0].show()
+
 
 
 if __name__=="__main__":
     test()
+    # test2()
